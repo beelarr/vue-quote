2f256225645c539625fa88818dc5a120e138fde1
Bryon Larrance <Bryon.Larrance@smiledirectclub.com>
2018-11-14 11:28:51 -0600

    Init through add quote

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <new-quote @quoteAdded="postNewQuote"></new-quote>
+        <quote-grid :quotes="quotes" ></quote-grid>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+
+    export default {
+        data: () => ({
+            quotes: [
+                'Just a quote to see.'
+            ],
+            maxQuotes: 10
+          }),
+      methods: {
+        	postNewQuote(quote){
+        		this.quotes.push(quote)
+          }
+      },
+      components: {
+        	quoteGrid: QuoteGrid,
+          newQuote: NewQuote
+      }
+    }
+</script>
+
+<style>
+</style>
